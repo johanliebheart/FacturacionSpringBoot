5cbd0bbd35cd35e89b3a88bcc195751cf14c01c8
Erick González <itenhuck@gmail.com>
2020-09-28 14:09:20 -0500

    Spring Security + JDBC (sin JPA)
    se debe crear dos tablas, una user y otra authorities para que funcione.

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
@@ -1,7 +1,8 @@
 package com.bolsadeideas.springboot.app;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -21,13 +22,16 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	private LoginSuccesHandler succesHandler;
 	
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 
+	@Autowired
+	private DataSource dataSource;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		
+		
 		
 		http.authorizeRequests().antMatchers("/","/css/**","/js/**","/img/**","/listar").permitAll().
 		//antMatchers("/ver/**").hasAnyRole("USER").
@@ -49,18 +53,26 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	public void configurerGlobal(AuthenticationManagerBuilder builder) throws Exception {
 		
-		PasswordEncoder encoder = passwordEncoder();
+		//PasswordEncoder encoder = passwordEncoder;
 		/*
 		UserBuilder users = User.builder().passwordEncoder(password ->{
 			return encoder.encode(password);
 		}) ;
 		*/
 		//simplificación usando las características de java 8
-		UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+	/*	UserBuilder users = User.builder().passwordEncoder(encoder::encode);
 		
 		builder.inMemoryAuthentication()
 		.withUser(users.username("admin").password("12345").roles("ADMIN","USER"))
 		.withUser(users.username("andres").password("12345").roles("USER"));
+		*/
+		
+		builder.jdbcAuthentication()
+		.dataSource(dataSource)
+		.passwordEncoder(passwordEncoder)
+		.usersByUsernameQuery("select username, password, enable from users where username=?")
+		.authoritiesByUsernameQuery("select u.username, a.authority from authorities a inner join users u  on  (a.user_id=u.id) where u.username =?");
+		
 		
 	}
 	
